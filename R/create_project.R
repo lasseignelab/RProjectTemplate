51a0f7e472335742e5dbd05dab48134dab6c66b2
Amanda D. Clark <adc0032@auburn.edu>
2023-02-23 13:02:46 -0600

    adding git project setup

--- R/create_project.R
+++ R/create_project.R
@@ -11,7 +11,8 @@ create_project <- function(path, ...) {
 
   # rest of the function below
   demoPath_sys <- function (..., lib.loc = NULL, mustWork = FALSE){
-    system.file(..., package = "RProjectTemplate", lib.loc = lib.loc, mustWork = mustWork)
+    system.file(..., package = "RProjectTemplate",
+                lib.loc = lib.loc, mustWork = mustWork)
   }
 
   from <- demoPath_sys("LasseigneRprojTemplate")
@@ -22,7 +23,7 @@ create_project <- function(path, ...) {
     overwrite = TRUE
   )
 
-  # utility funciton to loop through all the files we need to make replacements in
+  # Function to loop through all the files we need to make replacements in
   replace_package_name <- function(copied_files,
                                    package_name,
                                    path){
@@ -62,7 +63,56 @@ create_project <- function(path, ...) {
     path
   )
 
+  setwd(path)
+
   if (dots$git) {
-    git2r::init(path)
+    git2r::init(path = ".", branch = "main")
+    git2r::add(path = ".")
+    git2r::commit(repo = ".", message = "Initial Project Commit")
+    #git2r::remote_add(name = basename(path), url = paste0("https://github.com/lasseignelab/",basename(path),".git"))
+  }
+
+  if (dots$createGitignore){
+    git_ignores <-
+      c("# History files",
+        ".Rhistory",
+        ".Rapp.history",
+        "\n",
+        "# Session Data files",
+        ".RData",
+        "\n",
+        "# User-specific files",
+        ".Ruserdata",
+        "\n",
+        " # Example code in package build process",
+        "*-Ex.R",
+        "\n",
+        " # Output files from R CMD build",
+    "/*.tar.gz",
+    "\n",
+    "# Output files from R CMD check",
+    "/*.Rcheck/",
+    "\n",
+      "# RStudio files",
+      ".Rproj.user/",
+      "\n" ,
+      "# produced vignettes",
+     " vignettes/*.html",
+   " vignettes/*.pdf",
+   "\n",
+    "# OAuth2 token, see https://github.com/hadley/httr/releases/tag/v0.3",
+    ".httr-oauth",
+    "\n",
+    "# knitr and R markdown default cache directories",
+    "*_cache/",
+      "/cache/",
+      "\n" ,
+     " # Temporary files created by R markdown",
+      "*.utf8.md",
+    "*.knit.md",
+    "\n",
+    "# R Environment Variables",
+    ".Renviron")
+    writeLines(paste(git_ignores, sep = '\n'), '.gitignore')
   }
 }
